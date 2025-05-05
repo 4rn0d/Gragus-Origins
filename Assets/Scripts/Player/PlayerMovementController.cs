cc27354675704407d09fec7b9473e5d5110d7202
Arnaud Turcotte <arnaudt09@gmail.com>
2025-05-05 13:52:32 -0400

    FCT : Ajout jump

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+// ReSharper disable All
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,12 +10,40 @@ public class PlayerController : MonoBehaviour
     
     [Header("Player Settings")]
     [SerializeField] float moveSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] float jumpMovementReductionForce;
+    
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheck;
 
     private float _horizontal;
 
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(_horizontal * moveSpeed, rb.linearVelocity.y);
+        if (_horizontal < 0)
+        {
+            gameObject.transform.localScale = new Vector2(0.25f, 0.25f);
+        }
+        if (_horizontal > 0)
+        {
+            gameObject.transform.localScale = new Vector2(-0.25f, 0.25f);
+        }
+    }
+
+    public bool IsGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+    }
+    
+    public void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump");
+        Debug.Log(IsGrounded());
+        if (context.performed && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x * jumpMovementReductionForce, jumpForce);
+        }
     }
     
     public void Move(InputAction.CallbackContext context)
