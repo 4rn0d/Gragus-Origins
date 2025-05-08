caaa15c16465ffda7e611e0187790d50b9190b78
Arnaud Turcotte <arnaudt09@gmail.com>
2025-05-08 12:18:02 -0400

    FCT : Ajout dash

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -13,14 +13,27 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float jumpForce;
     [SerializeField] float jumpMovementReductionForce;
     
-    [SerializeField] LayerMask groundLayer;
+    [Header("Dash Settings")]
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashDuration;
+    [SerializeField] float bounceVerticalBoost;
+    
     [SerializeField] Transform groundCheck;
+    [SerializeField] Transform wallCheck;
+    [SerializeField] LayerMask wallLayer;
+    [SerializeField] LayerMask groundLayer;
 
     private float _horizontal;
+    private bool _isDashing;
+    private float _dashTimer;
+    private float _dashDirection;
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(_horizontal * moveSpeed, rb.linearVelocity.y);
+        if (!_isDashing)
+        {
+            rb.linearVelocity = new Vector2(_horizontal * moveSpeed, rb.linearVelocity.y);
+        }
         if (_horizontal < 0)
         {
             gameObject.transform.localScale = new Vector2(0.25f, 0.25f);
@@ -29,11 +42,43 @@ public class PlayerController : MonoBehaviour
         {
             gameObject.transform.localScale = new Vector2(-0.25f, 0.25f);
         }
+        
+        if (_isDashing)
+        {
+            
+            rb.linearVelocity = new Vector2(_dashDirection * dashSpeed, 0);
+            if (IsTouchingWall())
+            {
+                _dashDirection *= -1;
+                _horizontal *= -1;;
+                
+                Vector3 scale = transform.localScale;
+                scale.x *= -1;
+                transform.localScale = scale;
+                
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, bounceVerticalBoost);
+                
+                _isDashing = false;
+            }
+            _dashTimer -= Time.fixedDeltaTime;
+
+            if (_dashTimer <= 0)
+            {
+                _isDashing = false;
+                rb.linearVelocity = Vector2.zero;
+            }
+        }
+
     }
 
     public bool IsGrounded()
     {
         return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+    }
+
+    public bool IsTouchingWall()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
     }
     
     public void Jump(InputAction.CallbackContext context)
@@ -45,9 +90,23 @@ public class PlayerController : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocity.x * jumpMovementReductionForce, jumpForce);
         }
     }
+
+    
+    public void Dash(InputAction.CallbackContext context)
+    {
+        if (context.performed && !_isDashing)
+        {
+            _isDashing = true;
+            _dashTimer = dashDuration;
+            _dashDirection = _horizontal;
+        }
+    }
     
     public void Move(InputAction.CallbackContext context)
     {
-        _horizontal = context.ReadValue<Vector2>().x;
+        if (!_isDashing)
+        {
+            _horizontal = context.ReadValue<Vector2>().x;
+        }
     }
 }
