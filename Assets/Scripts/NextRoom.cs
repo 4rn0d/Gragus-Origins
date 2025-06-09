6e5af1063e62a3a542f4a79d90bcdc34f7c9fa8c
Gabriel <gallaire2005@outlook.com>
2025-06-09 13:57:53 -0400

    FCT : Script pour plusieurs cameras (changement de salle)

---
+++ Assets/Scripts/NextRoom.cs
@@ -0,0 +1,22 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class NextRoom : MonoBehaviour
+{ 
+    
+    public CinemachineCamera currentCam;
+    public CinemachineCamera nextCam;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            currentCam.Priority = 0;
+            nextCam.Priority = 10;  // Higher value activates this one
+            Debug.Log("Switched to next camera");
+
+            (currentCam, nextCam) = (nextCam, currentCam);
+        }
+    }
+    
+}
