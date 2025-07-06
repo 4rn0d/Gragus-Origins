9158a892e748eadbbf0e2d325d35eea8d536d3c0
Arnaud Turcotte <arnaudt09@gmail.com>
2025-07-06 15:53:06 -0400

    FCT : Ajout d'une page d'option et d'un slider pour le volume

---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Scripts
+{
+    public class SettingsMenu : MonoBehaviour
+    {
+        
+        [SerializeField] AudioMixer audioMixer;
+        
+        public void SetVolume(float volume)
+        {
+            audioMixer.SetFloat("volume", volume);
+        }
+    }
+}
