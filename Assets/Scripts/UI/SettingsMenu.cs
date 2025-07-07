ac6371a090f195057c831d0ccd072969d2eb7d6d
Arnaud Turcotte <arnaudt09@gmail.com>
2025-07-06 21:25:33 -0400

    FCT : implementation du code pour fullscreen et resolution

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 namespace Scripts
 {
@@ -8,6 +11,40 @@ namespace Scripts
         
         [SerializeField] AudioMixer audioMixer;
         
+        [SerializeField] Dropdown resolutionDropdown;
+        
+        private Resolution[] _resolutions;
+
+        private void Start()
+        {
+            _resolutions = Screen.resolutions;
+            
+            resolutionDropdown.ClearOptions();
+            
+            List<string> options = new List<string>();
+            int currentResolutionIndex = 0;
+            for (int i = 0; i < _resolutions.Length; i++)
+            {
+                string option = _resolutions[i].width + " x " + _resolutions[i].height;
+                options.Add(option);
+
+                if (_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex = i;
+                }
+            }
+            
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
+        }
+        
+        public void SetResolution(int resolutionIndex)
+        {
+            Resolution resolution = _resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        }
+
         public void SetVolume(float volume)
         {
             audioMixer.SetFloat("volume", volume);
