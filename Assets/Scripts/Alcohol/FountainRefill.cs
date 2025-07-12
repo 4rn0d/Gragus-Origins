6d2c41547e39b745a7ade118d3caca91db7b5f3b
Ulrich <2235274@cegepmontpetit.ca>
2025-07-12 12:15:49 -0400

    FCT UI Interaction
    Toujours utilisation illimite

--- Assets/Scripts/Alcohol/FountainRefill.cs
+++ Assets/Scripts/Alcohol/FountainRefill.cs
@@ -9,25 +9,36 @@ namespace Alcohol
 {
     public class FountainRefill : MonoBehaviour
     {
+        [SerializeField] private GameObject fPromptUI;
+        [SerializeField] private SpriteRenderer fountainRenderer;
         [SerializeField] private float refillAmount = 4f;
         [SerializeField] private AudioClip refillSound;
         private static PlayerController _playerController;
+        [SerializeField] private bool _isUsed;
+
+        private void Start()
+        {
+            fPromptUI.SetActive(false);
+            _isUsed = false;
+        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.CompareTag("Player"))
+            if (other.CompareTag("Player") && !_isUsed)
             {
                 _playerController = other.GetComponent<PlayerController>();
                 _playerController.triggerActive = true;
+                fPromptUI.SetActive(true);
             }
         }
 
-        public void OnTriggerExit(Collider other)
+        public void OnTriggerExit2D(Collider2D other)
         {
             if (other.CompareTag("Player"))
             {
                 _playerController.triggerActive = false;
                 _playerController = null;
+                fPromptUI.SetActive(false);
                 
             }
         }
@@ -38,13 +49,19 @@ namespace Alcohol
 
             if (_playerController != null && _playerController.triggerActive)
             {
-                transform.parent.gameObject.SetActive(false);
+                _playerController.triggerActive = false;
                 _playerController.RefillAlcohol(refillAmount);
-
+                _isUsed = true;
                 SoundFXManager.instance.PlaySoundFXClip(refillSound, transform, 1f);
             }
         }
 
-
+        private void Update()
+        {
+            if (_isUsed)
+            {
+                fountainRenderer.color = Color.gray;  
+            }
+        }
     }
 }
