407461bb4b746bba1c3d09aca1b09f843ee2bb28
Gabriel <gallaire2005@outlook.com>
2025-07-13 11:12:07 -0400

    FCT : Son pour attaque du Gromp

--- Assets/Scripts/Enemy/MeleeDamage.cs
+++ Assets/Scripts/Enemy/MeleeDamage.cs
@@ -7,7 +7,8 @@ public class MeleeDamage : MonoBehaviour
     [SerializeField] private float damageToPlayer = 25f;
     [SerializeField] private float damageToEnemy = 25f;
     [SerializeField] private AudioClip dashDamageSound;
-    
+    [SerializeField] private AudioClip meleeHitSound;
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -26,7 +27,7 @@ public class MeleeDamage : MonoBehaviour
                 if (enemyHealth != null)
                 {
                     //move cette logique dans gragus
-                    SoundFXManager.instance.PlaySoundFXClip(dashDamageSound,transform,1f);
+                    SoundFXManager.instance.PlaySoundFXClip(dashDamageSound, transform, 1f);
                     enemyHealth.TakeDamage(damageToEnemy);
                     Debug.Log("Player dashed into enemy — enemy took damage.");
                 }
@@ -35,6 +36,7 @@ public class MeleeDamage : MonoBehaviour
             {
                 // Player is not dashing → damage the player
                 playerHealth.TakeDamage(damageToPlayer);
+                SoundFXManager.instance.PlaySoundFXClip(meleeHitSound, transform, 1f);
                 Debug.Log("Enemy hit player — player took damage.");
             }
         }
