2c2e73493d3fe473b0d7fc017f377be6b0b3b6ee
Gabriel <gallaire2005@outlook.com>
2025-08-04 21:00:54 -0400

    FCT : 2 Floors

--- Assets/Scripts/Map/DungeonGenerator.cs
+++ Assets/Scripts/Map/DungeonGenerator.cs
@@ -313,7 +313,7 @@ namespace Map
 
                     // SELECT ROOM TYPE BASED ON FLOOR
                     GameObject prefab;
-                    if (DungeonManager.Instance.GetCurrentFloor() == 3)
+                    if (DungeonManager.Instance.GetCurrentFloor() == 2)
                         prefab = GetBossRoomPrefabForDirection(door.direction);
                     else
                         prefab = GetFinalRoomPrefabForDirection(door.direction);
